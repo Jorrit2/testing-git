c6064940d71c05f4b8de96f45559037e5381feed
Jorrit2 <73249270+Jorrit2@users.noreply.github.com>
2020-10-21 20:36:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import time
 
 for i in range(0, 21):
   time.sleep(.7)
+  print("i now have the files on my pc and will update then to the repository using git 2.0")
   print("just testing git")
   print(f"wow{i}")
